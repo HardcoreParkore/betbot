77a8a58ee3ab68b4c36877f4061b5df26ff5706f
Parker Johnson <mrstormthebeach@gmail.com>
2019-05-26 13:51:49 -0600

    fix bugs from moving to mongo

---
+++ src/model/Bet.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+var ObjectId = mongoose.Schema.Types.ObjectId
+
+let BetSchema = new mongoose.Schema({
+  details: {
+    type: String
+  },
+  objId: {
+    type: ObjectId
+  }
+})
+
+let Bet = mongoose.model('Bet', BetSchema)
+
+export default Bet
