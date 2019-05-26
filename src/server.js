9f2fc4d80fc7b76928666792f1b46b500cd7795f
Parker Johnson <mrstormthebeach@gmail.com>
2019-05-26 13:56:55 -0600

    improve logging

--- src/server.js
+++ src/server.js
@@ -1,32 +1,32 @@
-import express from 'express'
-import path from 'path'
-import fs from 'fs'
-import mongoose from 'mongoose'
-import assert from 'assert'
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import mongoose from 'mongoose';
+import assert from 'assert';
 
-const port = process.env.PORT || 1338
-const hookUrl = process.env.SLACK_HOOK_URL
-const mongoUri = process.env.MONGODB_URI
-const mongoDbName = process.env.MONGODB_NAME
+const port = process.env.PORT || 1338;
+const hookUrl = process.env.SLACK_HOOK_URL;
+const mongoUri = process.env.MONGODB_URI;
+const mongoDbName = process.env.MONGODB_NAME;
 
-const app = express()
+const app = express();
 
-let Schema = mongoose.Schema
+let Schema = mongoose.Schema;
 
-import Bet from './model/Bet'
+import Bet from './model/Bet';
 
-mongoose.connect(mongoUri)
-mongoose.Promise = global.Promise // Weird
+mongoose.connect(mongoUri);
+mongoose.Promise = global.Promise; // Weird
 
-let db = mongoose.connection
+let db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', function() {
-  console.log('connection open!!')
-})
+  console.log('connection open!!');
+});
 
 app.post('/bet', (req, res) => {
-  console.info(req.body)
+  console.info(req);
 
   Bet.insertOne(
     {
@@ -36,33 +36,33 @@ app.post('/bet', (req, res) => {
       if (err) {
         res
           .status(500)
-          .send('Something went wrong submitting the bet', err, data)
+          .send('Something went wrong submitting the bet', err, data);
       } else {
-        res.status(200).send('Successfully created the bet!')
+        res.status(200).send('Successfully created the bet!');
       }
     }
-  )
-})
+  );
+});
 
 app.post('/bets', (req, res) => {
   Bet.find({}, (err, bets) => {
-    let data = []
+    let data = [];
     bets.forEach(bet => {
-      data.push({ betDetails: bet.details })
-    })
-    res.status(200).send(data)
-  })
-})
+      data.push({ betDetails: bet.details });
+    });
+    res.status(200).send(data);
+  });
+});
 
 app.post('/betkill', (req, res) => {
-  console.info(req)
-  res.send('complete/finish a bet')
-})
+  console.info(req);
+  res.send('complete/finish a bet');
+});
 
 app.get('/isalive', (req, res) => {
-  res.send(true)
-})
+  res.send(true);
+});
 
 app.listen(port, () => {
-  console.log('App listening on port ' + port + '.')
-})
+  console.log('App listening on port ' + port + '.');
+});
