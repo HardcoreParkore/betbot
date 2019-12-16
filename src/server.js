870829028c9476c83f527762f7776f4c7910d5c7
Parker Johnson <mrstormthebeach@gmail.com>
2019-12-16 08:07:50 -0700

    add usernames to old bet listings

--- src/server.js
+++ src/server.js
@@ -71,7 +71,15 @@ app.post('/bets', (req, res) => {
     let attachments = [];
     bets.forEach(bet => {
       if (bet.details) {
-        let betDetails = '[' + bet.id + '](' + bet.status + '): ' + bet.details;
+        let betDetails =
+          'From: ' +
+          bet.metadata.user_name +
+          '[' +
+          bet.id +
+          '](' +
+          bet.status +
+          '): ' +
+          bet.details;
         attachments.push({ text: betDetails });
       }
     });
